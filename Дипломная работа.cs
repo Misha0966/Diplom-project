04c271fc824d37339165c977cd957bfded62695b
Михаил <108358743+Misha0966@users.noreply.github.com>
2023-11-27 09:12:36 +0300

    Update Дипломная работа.cs
    внёс некоторые коррективы

--- Дипломная работа.cs
+++ Дипломная работа.cs
@@ -9,9 +9,7 @@ class Program
         double Vn = ((Math.PI * h) / 6) * (3 * r * r + h * h);  // объем наземной части муравейника, где r - радиус муравейника, а h - его высота
         double Vp = 2.0 / 3.0 * Vn;  // объем поземной части муравейника
         double Vreal = Vn + Vp;  // где Vn - объем наземной части, а Vp - объем поземной части
-
         Console.WriteLine("Общий объем муравейника: " + Vreal);
-
         // Определим размерность выборки
         double[] measurements = { 0.003, 0.003, 0.003, 0.003, 0.01, 0.003, 0.004, 0.003, 0.004, 0.003 };
         double Na = 0;
@@ -21,11 +19,9 @@ class Program
         }
         Na /= 10.0;
         Console.WriteLine("Размер выборки: " + Na);
-
         // Определим объём выборки
         double VNa = Na * Math.Pow(10, -6);
         Console.WriteLine("Объем выборки: " + VNa);
-
         // Определим кол-во муравьёв, живущих в муравейнике
         double x = (Vreal / VNa) / 10.0;
         int roundedX = (int)Math.Round(x);
